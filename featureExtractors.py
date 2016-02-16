52cf4e5990c5a9271c5cd66e30dbfe895e69a993
Besermenji <besermenji.vladimir@gmail.com>
2016-02-16 22:35:38 +0100

    wrote a custom feature extractor and tweaked it so now pacman eats capsules and eats ghosts if they are in his way

--- featureExtractors.py
+++ featureExtractors.py
@@ -1,104 +1,209 @@
 # featureExtractors.py
 # --------------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+# Licensing Information: Please do not distribute or publish solutions to this
+# project. You are free to use and extend these projects for educational
+# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
+# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
-
-"Feature extractors for Pacman game states"
+"""Feature extractors for Pacman game states"""
 
 from game import Directions, Actions
 import util
 
-class FeatureExtractor:
-    def getFeatures(self, state, action):
-        """
-          Returns a dict from features to counts
-          Usually, the count will just be 1.0 for
-          indicator functions.
-        """
-        util.raiseNotDefined()
+class FeatureExtractor:  
+  def getFeatures(self, state, action):    
+    """
+      Returns a dict from features to counts
+      Usually, the count will just be 1.0 for
+      indicator functions.  
+    """
+    util.raiseNotDefined()
 
 class IdentityExtractor(FeatureExtractor):
-    def getFeatures(self, state, action):
-        feats = util.Counter()
-        feats[(state,action)] = 1.0
-        return feats
-
-class CoordinateExtractor(FeatureExtractor):
-    def getFeatures(self, state, action):
-        feats = util.Counter()
-        feats[state] = 1.0
-        feats['x=%d' % state[0]] = 1.0
-        feats['y=%d' % state[0]] = 1.0
-        feats['action=%s' % action] = 1.0
-        return feats
+  def getFeatures(self, state, action):
+    feats = util.Counter()
+    feats[(state,action)] = 1.0
+    return feats
 
 def closestFood(pos, food, walls):
-    """
-    closestFood -- this is similar to the function that we have
-    worked on in the search project; here its all in one place
-    """
-    fringe = [(pos[0], pos[1], 0)]
-    expanded = set()
-    while fringe:
-        pos_x, pos_y, dist = fringe.pop(0)
-        if (pos_x, pos_y) in expanded:
-            continue
-        expanded.add((pos_x, pos_y))
-        # if we find a food at this location then exit
-        if food[pos_x][pos_y]:
-            return dist
-        # otherwise spread out from the location to its neighbours
-        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
-        for nbr_x, nbr_y in nbrs:
-            fringe.append((nbr_x, nbr_y, dist+1))
-    # no food found
-    return None
+  """
+  closestFood -- this is similar to the function that we have
+  worked on in the search project; here its all in one place
+  """
+  fringe = [(pos[0], pos[1], 0)]
+  expanded = set()
+  while fringe:
+    pos_x, pos_y, dist = fringe.pop(0)
+    if (pos_x, pos_y) in expanded:
+      continue
+    expanded.add((pos_x, pos_y))
+    # if we find a food at this location then exit
+    if food[pos_x][pos_y]:
+      return dist
+    # otherwise spread out from the location to its neighbours
+    nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+    for nbr_x, nbr_y in nbrs:
+      fringe.append((nbr_x, nbr_y, dist+1))
+  # no food found
+  return None
 
+def numCloseObjects(pos, objects, walls,max_dist):
+  fringe = [(pos[0], pos[1], 0)]
+  expanded = set()
+  num = 0
+  while fringe:
+    pos_x, pos_y, dist = fringe.pop(0)
+    if (pos_x, pos_y) in expanded:
+      continue
+    if dist > max_dist:
+      return num
+    expanded.add((pos_x, pos_y))
+    # if we find a target object at then add to num
+    if (pos_x,pos_y) in objects:
+        num += 1
+    # otherwise spread out from the location to its neighbours
+    nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+    for nbr_x, nbr_y in nbrs:
+      fringe.append((nbr_x, nbr_y, dist+1))
+  return 0
+
+def closestObject(pos, objects, walls):
+  fringe = [(pos[0], pos[1], 0)]
+  expanded = set()
+  while fringe:
+    pos_x, pos_y, dist = fringe.pop(0)
+    if (pos_x, pos_y) in expanded:
+      continue
+    expanded.add((pos_x, pos_y))
+    # if we find a target object at this location then exit
+    if (pos_x,pos_y) in objects:
+      return dist
+    # otherwise spread out from the location to its neighbours
+    nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+    for nbr_x, nbr_y in nbrs:
+      fringe.append((nbr_x, nbr_y, dist+1))
+  return None
+
+def distAllObject(pos, objects, walls):
+  fringe = [(pos[0], pos[1], 0)]
+  expanded = set()
+  all_distances = []
+  while fringe:
+    pos_x, pos_y, dist = fringe.pop(0)
+    if (pos_x, pos_y) in expanded:
+      continue
+    expanded.add((pos_x, pos_y))
+    # Found object
+    if (pos_x,pos_y) in objects:
+      all_distances.append(dist)
+      objects.remove((pos_x,pos_y))
+      if not objects:
+        return all_distances
+    # otherwise spread out from the location to its neighbours
+    nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+    for nbr_x, nbr_y in nbrs:
+      fringe.append((nbr_x, nbr_y, dist+1))
+  if all_distances:
+    return all_distances
+  return None
+
+  
 class SimpleExtractor(FeatureExtractor):
-    """
-    Returns simple features for a basic reflex Pacman:
-    - whether food will be eaten
-    - how far away the next food is
-    - whether a ghost collision is imminent
-    - whether a ghost is one step away
-    """
+  """
+  Returns simple features for a basic reflex Pacman:
+  - whether food will be eaten
+  - how far away the next food is
+  - whether a ghost collision is imminent
+  - whether a ghost is one step away
+  """
+  
+  def getFeatures(self, state, action):
+    # extract the grid of food and wall locations and get the ghost locations
+    food = state.getFood()
+    walls = state.getWalls()
+    ghosts = state.getGhostPositions()
 
-    def getFeatures(self, state, action):
-        # extract the grid of food and wall locations and get the ghost locations
-        food = state.getFood()
-        walls = state.getWalls()
-        ghosts = state.getGhostPositions()
-		capsules = state.getCapsules()
+    features = util.Counter()
+    
+    features["bias"] = 1.0
+    
+    # compute the location of pacman after he takes the action
+    x, y = state.getPacmanPosition()
+    dx, dy = Actions.directionToVector(action)
+    next_x, next_y = int(x + dx), int(y + dy)
+    
+    # count the number of ghosts 1-step away
+    features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
 
-        features = util.Counter()
+    # if there is no danger of ghosts then add the food feature
+    if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
+      features["eats-food"] = 1.0
 
-        features["bias"] = 1.0
+    dist = closestFood((next_x, next_y), food, walls)
+    if dist is not None:
+      # make the distance a number less than one otherwise the update
+      # will diverge wildly
+      features["closest-food"] = float(dist) / (walls.width * walls.height) 
+    features.divideAll(10.0)
+    return features
 
-        # compute the location of pacman after he takes the action
-        x, y = state.getPacmanPosition()
-        dx, dy = Actions.directionToVector(action)
-        next_x, next_y = int(x + dx), int(y + dy)
 
-        # count the number of ghosts 1-step away
-        features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
 
-        # if there is no danger of ghosts then add the food feature
-        if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
-            features["eats-food"] = 1.0
+class CustomExtractor(FeatureExtractor):
+  def getFeatures(self, state, action):
+    foodpos = state.getFood()
+    foodpos = foodpos.asList()
+    walls = state.getWalls()
+    capsules = state.getCapsules()
+    ghosts = state.getGhostPositions()
 
-        dist = closestFood((next_x, next_y), food, walls)
-        if dist is not None:
-            # make the distance a number less than one otherwise the update
-            # will diverge wildly
-            features["closest-food"] = float(dist) / (walls.width * walls.height)
-        features.divideAll(10.0)
-        return features
+    SimpleExtractorObject = SimpleExtractor()
+    features = SimpleExtractorObject.getFeatures(state,action)
+
+    # Calculating pacmans position after current action
+    x, y = state.getPacmanPosition()
+    dx, dy = Actions.directionToVector(action)
+    next_x, next_y = int(x + dx), int(y + dy)
+    
+    ghostStates =  state.getGhostStates()
+    
+    time_left_for_all_neighbour_ghosts = []
+    min_time_left_for_all_neighbour_ghosts = 0
+
+    # Find time left for all neighbouring ghosts
+    for ghost in ghostStates:
+        if (next_x,next_y) in Actions.getLegalNeighbors(ghost.getPosition(),walls):
+            time_left_for_all_neighbour_ghosts += [ghost.scaredTimer]
+
+    # Find min time left for ghosts to remain eatable
+    if time_left_for_all_neighbour_ghosts:
+        min_time_left_for_all_neighbour_ghosts = min(time_left_for_all_neighbour_ghosts)
+    if min_time_left_for_all_neighbour_ghosts > 1:
+        features["can-i-eat-ghosts"] = 1
+        features["#-of-ghosts-1-step-away"] = 0
+
+    # dist of closest ghost - (slow ?) A useless feature ?
+    tot_size = walls.width*walls.height
+    num_close_ghosts = numCloseObjects((next_x, next_y), ghosts, walls,3)
+    temp = (1.0 + num_close_ghosts) / 10
+    features["no-of-close-ghosts"] = num_close_ghosts / 10.0
+
+    # dist of closest capsule (slow)
+    dist = closestObject((next_x, next_y), capsules, walls)
+    if dist is not None:
+         features["closest-capsule"] = float(dist) / (walls.width * walls.height)
+
+
+    # Possibly trapped
+    poss_actions =  Actions.getLegalNeighbors((next_x,next_y),walls)
+    poss_actions = set(poss_actions)
+    poss_ghost_positions = []
+    for g in ghosts:
+        poss_ghost_positions += Actions.getLegalNeighbors(g, walls)
+    remaining_action = poss_actions - set(poss_ghost_positions)
+    if not remaining_action:
+        features["trapped"] = 1 / 10.0
+
+
+    return features
